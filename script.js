c87b6b1d3e25ae75668a64423bdde70cc8a3b482
irfan <ipekusic@yahoo.com>
2022-10-03 11:00:04 -0400

    enjoy

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Your code here
+const table = document.getElementById("graph")
+function makeRow(){
+   let row = document.createElement("tr")
+   for (let i = 0; i < 20; i++){
+    let td = document.createElement('td')
+    row.appendChild(td)
+   }
+   table.appendChild(row)
+}
+
+makeRow()
+makeRow()
